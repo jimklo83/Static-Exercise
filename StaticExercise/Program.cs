0e204aec87a00521497256961a502b7766223d9c
jimklo83 <jimklo83@mail.com>
2023-10-10 14:36:48 -0500

    Finished

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"{TempConverter.FahrenheitToCelsius(0)}");
+            Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(17)}");
         }
     }
 }
